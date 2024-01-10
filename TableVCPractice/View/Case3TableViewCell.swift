6dc84d091ead6ffe01c6a22e5441c18b0ebab47c
Jin0331 <sempre813@naver.com>
2024-01-10 22:37:54 +0900

    [Case3] protocol 및 extension 적용....

--- TableVCPractice/View/Case3TableViewCell.swift
+++ TableVCPractice/View/Case3TableViewCell.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 
-class Case3TableViewCell: UITableViewCell {
+protocol Case3UiConfigure {
+    func setCellDesign()
+}
+
+class Case3TableViewCell: UITableViewCell, Case3UiConfigure{
     
     @IBOutlet weak var backgroundLabel: UILabel!
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var cellTextLabel: UILabel!
     
-}
+    func setCellDesign() {
+        cellTextLabel.font = .boldSystemFont(ofSize: 11)
+        leftButton.tintColor = .black
+        rightButton.tintColor = .black
 
+    }
+}

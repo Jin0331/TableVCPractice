284c4886f406fe5b1af4cb73034f5bf42942c3e2
Jin0331 <sempre813@naver.com>
2024-01-10 17:37:30 +0900

    [Case2] Controller에서 사용되는 데이터를 Struct로 바꿈

--- TableVCPractice/Controller/Case2TableViewController.swift
+++ TableVCPractice/Controller/Case2TableViewController.swift
@@ -9,9 +9,7 @@ import UIKit
 
 class Case2TableViewController: UITableViewController {
 
-    let headerList : [String] = ["전체 설정", "개인 설정", "기타"]
-    let dataList : [[String]] = [["공지사항", "실험실", "버전 정보"], 
-    ["개인/보안", "알림", "채팅", "멀티프로필"], ["고객센터/도움말"]]
+    var settingList = SettingList()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,26 +19,26 @@ class Case2TableViewController: UITableViewController {
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         
-        return headerList.count
+        return settingList.list.count
 
     }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         
-        return headerList[section]
+        return settingList.list[section].headerList
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         
-        return dataList[section].count
+        return settingList.list[section].dataList.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Case2TableViewCell", for: indexPath)
 
-        cell.textLabel?.text = dataList[indexPath.section][indexPath.row]
-        
+        cell.textLabel?.text = settingList.list[indexPath.section].dataList[indexPath.row]
+                
         return cell
     }
 

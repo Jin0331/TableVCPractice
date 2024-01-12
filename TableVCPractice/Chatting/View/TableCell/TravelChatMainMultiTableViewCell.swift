132f0b6fd0da566de2c94c2e2a59e8723db6245c
Jin0331 <sempre813@naver.com>
2024-01-12 17:11:51 +0900

    [Add] Chatting 과제를 위한 파일추가 및 파일 구조 설정

---
+++ TableVCPractice/Chatting/View/TableCell/TravelChatMainMultiTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelChatMainMultiTableViewCell.swift
+//  TableVCPractice
+//
+//  Created by JinwooLee on 1/12/24.
+//
+
+import UIKit
+
+class TravelChatMainMultiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
